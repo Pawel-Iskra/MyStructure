2998128c67aa14954e564c783f886616541d709a
Pawel-Iskra <paweliszcz@wp.pl>
2020-10-23 13:57:06 +0200

    [MyStructure] my first implementatnion

--- src/main/java/my_structure/MyStructure.java
+++ src/main/java/my_structure/MyStructure.java
@@ -1,23 +1,36 @@
 package my_structure;
 
 import java.util.List;
+import java.util.Optional;
 
 public class MyStructure implements IMyStructure {
     private List<INode> nodes;
 
+    public boolean add(INode node) {
+        return nodes.add(node);
+    }
 
     @Override
     public INode findByCode(String code) {
-        return null;
+        Optional<INode> foundNode = nodes.stream()
+                .filter(node -> code.equals(node.getCode()))
+                .findFirst();
+        if (foundNode.isEmpty()) return null;
+        return foundNode.get();
     }
 
     @Override
     public INode findByRenderer(String renderer) {
-        return null;
+        Optional<INode> foundNode = nodes.stream()
+                .filter(node -> renderer.equals(node.getRenderer()))
+                .findFirst();
+        if (foundNode.isEmpty()) return null;
+        return foundNode.get();
     }
 
     @Override
     public int count() {
-        return 0;
+        return nodes.size();
     }
+
 }
---
+++ src/main/java/my_structure/Node.java
@@ -0,0 +1,38 @@
+package my_structure;
+
+import java.util.Objects;
+
+public class Node implements INode{
+    private String code;
+    private String renderer;
+
+    public Node(String code, String renderer) {
+        this.code = code;
+        this.renderer = renderer;
+    }
+
+
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getRenderer() {
+        return renderer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return Objects.equals(code, node.code) && Objects.equals(renderer, node.renderer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, renderer);
+    }
+}
